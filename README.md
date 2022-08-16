b254ae82676975dd666e7b70e9fd15829c5dadb8
Claudio Martínez Ortiz <46463001+CMOSiemo@users.noreply.github.com>
2022-08-16 03:13:36 -0400

    v1.0

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# _Substrings exercise_
+# _Substrings exercise for TOP_
 
 #### By _**Claudio Martínez Ortiz**_
 
 ## Description
 
-#### _Substrings exercise from [TOP](https://www.theodinproject.com/lessons/ruby-sub-strings)_
+#### _Substrings exercise for [TOP](https://www.theodinproject.com/lessons/ruby-sub-strings)_
 
 ## Technologies Used
 
 * _Ruby_
+
+Copyright (c) 2022 Claudio Martínez Ortiz
--- substrings.rb
+++ substrings.rb
@@ -1,1 +1,48 @@
-   
+#Copyright (c) 2022 Claudio Martínez Ortiz
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def curatext_to_array(text)
+
+  #Replaces common irregular spanish characters with analog, downcase and split
+  holder = text.gsub("á", "a").gsub("é", "e").gsub("í", "i").gsub("ó", "o").gsub(
+  "ú", "u").gsub("Á", "A").gsub("É", "E").gsub("Í", "I").gsub("Ó", "O").gsub("Ú",
+  "U").downcase.split(/[\s[^A-Za-zÑñ']]/)
+  holder.delete("")
+
+  #Support 's as a word
+  holder.map! do |word|
+  
+    if word =~ /'/
+      splitter = word.split(/'/)
+      splitter[1] = "'s"
+      splitter
+    
+    else word
+    
+    end
+
+  end
+
+  holder.flatten
+
+end
+
+def substrings(text, dictionary)
+
+  #Curate text and store in an array
+  array = curatext_to_array(text)
+  
+  #Reduce to accumulate of elements as labels in a hash
+  array.reduce(Hash.new(0)) do |hash, word|
+    
+    #Add when included
+    dictionary.each { |entrance| hash[entrance] += 1 if word.include?(entrance) }
+    hash
+
+  end
+
+end
+
+input = "Howdy partner, sit down! How's it going?"
+p hash = substrings(input, dictionary)
