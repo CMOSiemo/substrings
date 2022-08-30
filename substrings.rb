ddd64f007ffa01bc11f290a006be49aea60bb08f
Claudio Martínez Ortiz <46463001+CMOSiemo@users.noreply.github.com>
2022-08-30 13:55:00 -0400

    v 1.02

--- substrings.rb
+++ substrings.rb
@@ -1,48 +1,46 @@
-#Copyright (c) 2022 Claudio Martínez Ortiz
+# frozen_string_literal: true
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# Copyright (c) 2022 Claudio Martínez Ortiz
 
-def curatext_to_array(text)
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-  #Replaces common irregular spanish characters with analog, downcase and split
-  holder = text.gsub("á", "a").gsub("é", "e").gsub("í", "i").gsub("ó", "o").gsub(
-  "ú", "u").gsub("Á", "A").gsub("É", "E").gsub("Í", "I").gsub("Ó", "O").gsub("Ú",
-  "U").downcase.split(/[\s[^A-Za-zÑñ']]/)
-  holder.delete("")
-
-  #Support 's as a word
-  holder.map! do |word|
-  
+def supportsword(word_array)
+  word_array.map! do |word|
     if word =~ /'/
       splitter = word.split(/'/)
       splitter[1] = "'s"
       splitter
-    
-    else word
-    
+
+    else
+      word
+
     end
-
   end
+end
 
-  holder.flatten
+def curatext_to_array(text)
+  # Replaces common irregular spanish characters with analog, downcase and split
+  word_array = text.gsub('á', 'a').gsub('é', 'e').gsub('í', 'i').gsub('ó', 'o').gsub('ú', 'u').gsub('Á', 'A')
+                   .gsub('É', 'E').gsub('Í', 'I').gsub('Ó', 'O').gsub('Ú', 'U').downcase.split(/[\s[^A-Za-zÑñ']]/)
+  word_array.delete('')
 
+  # Support 's as a word
+  supportsword(word_array)
+
+  word_array.flatten
 end
 
 def substrings(text, dictionary)
-
-  #Curate text and store in an array
+  # Curate text and store in an array
   array = curatext_to_array(text)
-  
-  #Reduce to accumulate elements as labels in a hash
-  array.reduce(Hash.new(0)) do |hash, word|
-    
-    #Add when included
+
+  # Reduce to accumulate elements as labels in a hash
+  array.each_with_object(Hash.new(0)) do |word, hash|
+    # Add when included
     dictionary.each { |entrance| hash[entrance] += 1 if word.include?(entrance) }
-    hash
-
   end
-
 end
 
 input = "Howdy partner, sit down! How's it going?"
-p hash = substrings(input, dictionary)
+puts input
+p substrings(input, dictionary)
